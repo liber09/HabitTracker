4d7b51b74b117b9ffb6c1cc49ae63151f2d79a02
liber09 <linda.bergsangel@hotmail.com>
2023-04-22 06:37:03 +0200

    working with SaveToDb implementation

--- Model/Habit.swift
+++ Model/Habit.swift
@@ -8,28 +8,18 @@
 import Foundation
 import FirebaseFirestoreSwift
 
-struct Habit:Codable,Identifiable{
+struct Habit : Codable, Identifiable{
     @DocumentID var id : String?
     var description: String
     var finished: Bool = false
     var streakDays: Int = 0
     var latest : Data?
     
-    private var unformatedDate = Date()
-    private let dateFormatter = DateFormatter()
-    
     init(
-        id: String? = nil, description: String, finished: Bool, category: String, streakDays: Int){
+        id: String? = nil, description: String, finished: Bool, streakDays: Int){
             self.description = description
             self.finished = finished
             self.streakDays = streakDays
-        }
-    
-    var date : String {
-            
-            dateFormatter.dateStyle = .medium
-            return dateFormatter.string(from: unformatedDate)
-            
         }
 }
 
